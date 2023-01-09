bd532ebcce3986d4d4d7f651bced0e18c2464671
alfa23 <paw235@gmail.com>
2023-01-09 15:49:46 +0000

    add: Webhook endpoint

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -41,7 +41,7 @@ def webhook(request):
     event_map = {
         'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
         'payment_intent.payment_failed': (
-            handler.handle_payment_intent_payment_failed),
+            handler.handle_payment_intent_payment_failed,)
     }
 
     # Get the webhook type from Stripe
